f32c0b1350f1102712d78dc3da1a87258adc2405
rephira <equinoxreal@gmail.com>
2017-05-20 13:39:58 +0100

    Add - config file - base connection setup - verification parameter read-in

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-// chihiro-bot source code
+//chihiro-bot source code
 
 'use strict';
 
+//requiring node modules
 const
   express = require('express'),
   bodyParser = require('body-parser'),
   request = require('request'),
-  config = require('config');
+  config = require('config'),
+  crypto = require('crypto'),
+  https = require('https');
 
+//base setup for connection
 let app = express();
+app.set('port', process.env.PORT || 5000); 
+app.set('view engine', 'ejs'); 
+//app.use(bodyParser.json({ check: checkReqSign }));
+app.use(express.static('public'));
 
-const trick = 4;
-const placeholder = (trick==config.get('aS')) ?
-  true :
-  false;
+//verification parameters
+const
+  APPSEC = (process.env.FBM_APPSEC) ?
+  process.env.FBM_APPSEC :
+  config.get('appSec'),
 
-console.log(placeholder)
+  VALTOKEN = (process.env.FBM_VALTOKEN) ?
+  process.env.FBM_VALTOKEN :
+  config.get('valToken'),
+
+  PAGETOKEN = (process.env.FBM_PAGETOKEN) ?
+  process.env.FBM_PAGETOKEN :
+  config.get('pageToken'),
+
+  SURL = (process.env.FBM_SURL) ?
+  process.env.FBM_SURL :
+  config.get('sURL');
